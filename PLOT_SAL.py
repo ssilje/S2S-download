c2c7d2660edcce224c2dc42c65f30ae0bb1bc729
Silje Sørland <ssor@norceresearch.no>
2021-02-26 16:55:41 +0100

    Update PLOT_SAL.py

--- PLOT_SAL.py
+++ PLOT_SAL.py
@@ -15,8 +15,8 @@ product = 'forecast' # forecast
 cycle = 'CY46R1'
 
 # Bergen
-lat = 60.23
-lon = 5.19
+lat = 65
+lon = 0
 
 dirbase_S2S = '/nird/projects/NS9001K/sso102/S2S/DATA/grib'
 dir = '%s/%s/%s/'%(dirbase_S2S,product,'/ECMWF/sfc')
@@ -31,26 +31,35 @@ for idate in dates_fcycle:
     d = idate.strftime('%Y-%m-%d')
     #(latitude=slice(50,30), longitude=slice(180,240))
     dS2S_cf = '%s/%s/%s_%s_%s_%s_%s%s'%(dir,var_short,var_short,cycle,d,'cf',product,'.grb')
-   # dataopen_cf = xr.open_dataset(dS2S_cf,engine='cfgrib').sel(latitude=lat, longitude=lon, method='nearest').to_dataframe() # Picking out a grid point
-    dataopen_cf = xr.open_dataset(dS2S_cf,engine='cfgrib').sel(latitude=slice(65,60), longitude=slice(0,5)).to_dataframe() # Picking out a grid point
+    dataopen_cf = xr.open_dataset(dS2S_cf,engine='cfgrib').sel(latitude=lat, longitude=lon, method='nearest').to_dataframe() # Picking out a grid point
+    #dataopen_cf = xr.open_dataset(dS2S_cf,engine='cfgrib').sel(latitude=slice(65,60), longitude=slice(0,5)).to_dataframe() # Picking out a grid point
     dS2S_pf = '%s/%s/%s_%s_%s_%s_%s%s'%(dir,var_short,var_short,cycle,d,'pf',product,'.grb')
-    dataopen_pf = xr.open_dataset(dS2S_pf,engine='cfgrib').sel(latitude=slice(65,60), longitude=slice(0,5)).to_dataframe() # Picking out a grid point
-    #dataopen_pf.reset_index(inplace=True)
-    #dataopen_cf.reset_index(inplace=True)  
-    data_append = dataopen_cf.append(dataopen_pf)
+    dataopen_pf = xr.open_dataset(dS2S_pf,engine='cfgrib').sel(latitude=lat, longitude=lon, method='nearest').to_dataframe() # Picking out a grid point
+
     
-  #  data_append.reset_index(inplace=True)  
-    if d == dates_monday[0].strftime('%Y-%m-%d'):
-        
-        data_all = data_append
-    else:                             
-        data_all = data_all.append(data_append) 
-    
-    #data_all.reset_index(inplace=True)  
-   # print('data_all')
-   # print(data_all.head(50)) # print the 20 first lines
-print('data_all')
-print(data_all.head(50)) # print the 20 first lines
+
+
+f, ax = plt.subplots(1, 1)
+for ens in range(1,51):
+    ax.plot(dataopen_pf.valid_time[dataopen_pf.sav300.index.get_level_values('number') == 1], dataopen_pf.sav300[dataopen_pf.sav300.index.get_level_values('number') == ens])
+#ax.plot(ERA5_BR.time, ERA5_BR.SST, color='0.1')
+#ax.plot(ERA5_BR.time, ERA5_BR.SST-ERA5_BR_std.SST, color='0.5')
+#ax.plot(ERA5_BR.time, ERA5_BR.SST+ERA5_BR_std.SST, color='0.5')
+#ax.plot(ERA5_BR.time, ERA5_BR.SST+ERA5_BR_std.SST, color='0.5')
+
+xfmt = mdates.DateFormatter('%d')
+ax.xaxis.set_major_formatter(xfmt)
+
+ax.set_xlabel('time')
+ax.set_ylabel('SALINITY')
+#ax.set_title('SST July', fontsize=16)
+
+
+
+#ERA5_BR.SST_std.plot()-ERA5_BR.SST
+#ERA5_BR.groupby(ERA5_BR_df.index.1).mean().plot()
+f.savefig('SALINITY.png')
+
 
 #for idata_all, rdata_all in data_all.iterrows(): 
 #    for iera, rera in ERA5_BR_daily.iterrows():   
