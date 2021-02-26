d77c6fe85bfa4bf0a9cd344ff7c860beedf80b84
Silje Sørland <ssor@norceresearch.no>
2021-02-26 19:07:06 +0100

    Update PLOT_SAL.py

--- PLOT_SAL.py
+++ PLOT_SAL.py
@@ -32,35 +32,40 @@ for idate in dates_fcycle:
     dir = '%s/%s/%s/'%(dirbase_S2S,product,'/ECMWF/sfc')
     dS2S_cf = '%s/%s/%s_%s_%s_%s_%s%s'%(dir,var_short,var_short,cycle,d,'cf',product,'.grb')
     dataopen_cf = xr.open_dataset(dS2S_cf,engine='cfgrib').sel(latitude=lat, longitude=lon, method='nearest').to_dataframe() # Picking out a grid point
+    #dataopen_cf.reset_index(inplace=True)  
     dS2S_pf = '%s/%s/%s_%s_%s_%s_%s%s'%(dir,var_short,var_short,cycle,d,'pf',product,'.grb')
     dataopen_pf = xr.open_dataset(dS2S_pf,engine='cfgrib').sel(latitude=lat, longitude=lon, method='nearest').to_dataframe() # Picking out a grid point
+    #dataopen_pf.reset_index(inplace=True)  
     
     product = 'hindcast' # forecast
     dir = '%s/%s/%s/'%(dirbase_S2S,product,'/ECMWF/sfc')
     dS2S_cf_hc = '%s/%s/%s_%s_%s_%s_%s%s'%(dir,var_short,var_short,cycle,d,'cf',product,'.grb')
     dataopen_cf_hc = xr.open_dataset(dS2S_cf_hc,engine='cfgrib').sel(latitude=lat, longitude=lon, method='nearest').to_dataframe() # Picking out a grid point
+    #dataopen_cf_hc.reset_index(inplace=True)  
     dS2S_pf_hc = '%s/%s/%s_%s_%s_%s_%s%s'%(dir,var_short,var_short,cycle,d,'pf',product,'.grb')
     dataopen_pf_hc = xr.open_dataset(dS2S_pf_hc,engine='cfgrib').sel(latitude=lat, longitude=lon, method='nearest').to_dataframe() # Picking out a grid point
-
+    #dataopen_pf_hc.reset_index(inplace=True) 
     
 
+forecast_length = pd.date_range(start="20200505", periods=46) # forecasts start Monday
 
-f, ax = plt.subplots(1, 1)
-for ens in range(1,11):
-    print(ens)
-    ax.plot(dataopen_pf_hc.valid_time[dataopen_pf_hc.valid_time.index.get_level_values('number') == ens], dataopen_pf_hc.sav300[dataopen_pf_hc.sav300.index.get_level_values('number') == ens],color='b',linewidth=0.2)
+#f, ax = plt.subplots(1, 1)
+#for ens in range(1,11):
+#    print(ens)
+    #ax.plot(dataopen_pf_hc.valid_time[dataopen_pf_hc.valid_time.index.get_level_values('number') == ens], dataopen_pf_hc.sav300[dataopen_pf_hc.sav300.index.get_level_values('number') == ens],color='b',linewidth=0.2)
+#    ax.plot(dataopen_pf_hc.index.get_level_values('step'), dataopen_pf_hc.sav300[dataopen_pf_hc.sav300.index.get_level_values('number') == ens],color='b',linewidth=0.2)
 
-ax.plot(dataopen_cf_hc.valid_time, dataopen_cf_hc.sav300, linewidth=0.2, color='b')
+#ax.plot(dataopen_cf.valid_time, dataopen_cf_hc.sav300, linewidth=0.2, color='b')
 
 
 for ens in range(1,51):
     print(ens)
-    ax.plot(dataopen_pf.valid_time[dataopen_pf.valid_time.index.get_level_values('number') == ens], dataopen_pf.sav300[dataopen_pf.sav300.index.get_level_values('number') == ens],color='r',linewidth=0.5)
-#ax.plot(ERA5_BR.time, ERA5_BR.SST, color='0.1')
-#ax.plot(ERA5_BR.time, ERA5_BR.SST-ERA5_BR_std.SST, color='0.5')
-#ax.plot(ERA5_BR.time, ERA5_BR.SST+ERA5_BR_std.SST, color='0.5')
-#ax.plot(ERA5_BR.time, ERA5_BR.SST+ERA5_BR_std.SST, color='0.5')
-ax.plot(dataopen_cf.valid_time, dataopen_cf.sav300, linewidth=2, color='k')
+    #ax.plot(dataopen_pf.valid_time[dataopen_pf.valid_time.index.get_level_values('number') == ens], dataopen_pf.sav300[dataopen_pf.sav300.index.get_level_values('number') == ens],color='r',linewidth=0.5)
+    #ax.plot(dataopen_pf.valid_time[dataopen_pf.valid_time.index.get_level_values('number') == ens], dataopen_pf.sav300[dataopen_pf.sav300.index.get_level_values('number') == ens] ,color='r',linewidth=0.5)
+    ax.plot(dataopen_pf.valid_time[dataopen_pf.valid_time.index.get_level_values('number') == ens].index.get_level_values('step'), dataopen_pf.sav300[dataopen_pf.sav300.index.get_level_values('number') == ens] ,color='r',linewidth=0.5)
+#ax.plot(dataopen_cf.valid_time, dataopen_cf.sav300, linewidth=2, color='k')
+ax.plot(dataopen_cf.index.get_level_values('step'), dataopen_cf.sav300, linewidth=2, color='k')
+
 xfmt = mdates.DateFormatter('%m%d')
 ax.xaxis.set_major_formatter(xfmt)
 
