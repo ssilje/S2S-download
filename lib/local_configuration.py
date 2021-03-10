57689106cbe2d386f385a224cbff2b79c8fa70b7
Silje Sørland <sso102@login0-nird-tos.nird.login.svc.cluster.local>
2021-03-10 09:51:15 +0100

    including Benjamins script

---
+++ lib/local_configuration.py
@@ -0,0 +1,5 @@
+config = {
+    'S2S_DIR': '/nird/projects/NS9001K/sso102/S2S/DATA/',
+    'BW_DIR': '/nird/projects/NS9001K/sso102/Python/Benjamin/',
+    'CF_DATA_DIR': '/nird/projects/NS9001K/sso102/Python/Benjamin/',
+}
