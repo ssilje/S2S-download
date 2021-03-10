ef117ad0da1c50310281eef17abb1508b590aa2b
Silje Sørland <ssor@norceresearch.no>
2021-03-10 10:06:13 +0100

    Update local_configuration.py

--- lib/local_configuration.py
+++ lib/local_configuration.py
@@ -1,5 +1,5 @@
 config = {
     'S2S_DIR': '/nird/projects/NS9001K/sso102/S2S/DATA/',
-    'BW_DIR': '/nird/projects/NS9001K/sso102/Python/Benjamin/',
-    'CF_DATA_DIR': '/nird/projects/NS9001K/sso102/Python/Benjamin/',
+    'BW_DIR': '/nird/projects/NS9001K/sso102/Python/S2S-download/Benjamin',
+    'CF_DATA_DIR': '/nird/projects/NS9001K/sso102/Python/S2S-download/Benjamin',
 }
