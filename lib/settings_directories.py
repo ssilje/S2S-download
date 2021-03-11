9d4457a20fd240efb751c52dce87500d49e82e19
Silje Sørland <ssor@norceresearch.no>
2021-03-11 15:05:35 +0100

    Update settings_directories.py

--- lib/settings_directories.py
+++ lib/settings_directories.py
@@ -1,7 +1,7 @@
 DIR = {
     'S2S_DIR': '/nird/projects/NS9853K/DATA/S2S',
     'SFE_DIR': '/nird/projects/NS9853K/DATA/SFE/Systems_daily_grib/',
-    'ERA5_daily_DIR': '/nird/projects/NS9853K/DATA/SFE/ERA_daily_nc/,
+    'ERA5_daily_DIR': '/nird/projects/NS9853K/DATA/SFE/ERA_daily_nc/',
     'ERA5_monthly_DIR': '/nird/projects/NS9853K/DATA/SFE/ERA_monthly_nc/',
     'S2S_DIR_summer2019': '/nird/projects/NS9853K/DATA/S2S/SUMMER2018',
     'SAVEFIG': ' /nird/projects/NS9001K/sso102/FIGUERS',
