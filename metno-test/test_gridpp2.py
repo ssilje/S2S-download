72d5f8a2e02bcd842f1a42a5da9231623e204cf5
Silje Sørland <sso102@login0-nird-tos.nird.login.svc.cluster.local>
2021-03-16 09:06:35 +0100

    testing point to point

--- metno-test/test_gridpp2.py
+++ metno-test/test_gridpp2.py
@@ -41,18 +41,45 @@ def read_grib_file(
     dataopen = xr.open_dataset(file_path, engine='cfgrib')
 
     return dataopen
-  
+
 def make_grid(lats, lons):
     latlats, lonlons = np.meshgrid(
         lats, lons
     )
     ECMWF_grid = gridpp.Grid(latlats, lonlons)
+    return ECMWF_grid
+
+
+## funkar ikkje
+def make_grid_flatten(lats, lons):
+    latlats, lonlons = np.meshgrid(
+        lats, lons
+    )
+    ECMWF_grid = gridpp.Grid(latlats.flatten(), lonlons.flatten())
     return ECMWF_grid 
   
   
-  
+def make_points(lats, lons,valid_points):
+    latlats, lonlons = np.meshgrid(
+        lats, lons
+    )
+    print(latlats.shape)
+    print(lonlons.shape)
+    print(valid_points.shape)
+    #valid_points=np.transpose(valid_points)
+    print(valid_points.shape)
+    print(latlats[valid_points].shape)
 
+#    ECMWF_points = gridpp.Points(latlats[valid_points], lonlons[valid_points])
+    ECMWF_points = gridpp.Points(latlats, lonlons)    
+    return ECMWF_points   
 
+def make_points_flatten(lats, lons,valid_points):
+    latlats, lonlons = np.meshgrid(
+        lats, lons
+    )
+    ECMWF_points = gridpp.Points(latlats.flatten()[valid_points], lonlons.flatten()[valid_points])
+    return ECMWF_points
 
 
 SAL_GRID1deg = read_grib_file(
@@ -85,6 +112,52 @@ SST_GRID1_5deg_EUR = read_grib_file(
 
 
 
+SST_GRID1_5deg_EUR = SST_GRID1_5deg 
+
+
+with open("metadata_BW_sites.json") as json_file:
+    data_BW = pd.DataFrame(json.load(json_file))
+BW_grid = gridpp.Points(data_BW.lat, data_BW.lon)
+print(data_BW.lat.shape)
 ECMWF_grid1_5deg_EUR = make_grid(SST_GRID1_5deg_EUR.latitude.data, SST_GRID1_5deg_EUR.longitude.data)
 ECMWF_grid1_5deg = make_grid(SST_GRID1_5deg.latitude.data, SST_GRID1_5deg.longitude.data)
 ECMWF_grid1deg = make_grid(SAL_GRID1deg.latitude.data, SAL_GRID1deg.longitude.data)
+#ECMWF_grid1_5deg_EUR = make_points(SST_GRID1_5deg_EUR.latitude.data, SST_GRID1_5deg_EUR.longitude.data)
+#ECMWF_grid1_5deg_EUR = make_grid_flatten(SST_GRID1_5deg_EUR.latitude.data, SST_GRID1_5deg_EUR.longitude.data)
+SST_1_5deg2point = gridpp.nearest(
+    ECMWF_grid1_5deg, 
+    BW_grid, 
+    gridpp.fill_missing(np.transpose(SST_GRID1_5deg.sst[0,:,:].data))
+)
+print(SST_1_5deg2point)
+
+sst = SST_GRID1_5deg_EUR.sst[0,:,:].data.flatten()
+valid_points = np.isnan(sst) == 0  # Ocean points
+
+ECMWF_points_EUR = make_points_flatten(SST_GRID1_5deg_EUR.latitude.data, SST_GRID1_5deg_EUR.longitude.data, valid_points) 
+
+
+ECMWF_points_EUR_v2 = gridpp.Points(ECMWF_grid1_5deg_EUR.get_lats().flatten()[valid_points],
+     ECMWF_grid1_5deg_EUR.get_lons().flatten()[valid_points])
+
+#SST_points_EUR_v2 = gridpp.nearest(ECMWF_points_EUR_v2, BW_grid, sst[valid_points])
+
+
+#SST_points_EUR = gridpp.nearest(ECMWF_points_EUR, BW_grid, sst[valid_points])
+
+
+
+sst = SST_GRID1_5deg_EUR.sst[0,:,:].data
+sst=np.transpose(sst)
+valid_points = np.isnan(sst) == 0  # Ocean points
+
+
+
+ECMWF_points_EUR = make_points(SST_GRID1_5deg_EUR.latitude.data, SST_GRID1_5deg_EUR.longitude.data, valid_points)
+# valid_points=np.transpose(valid_points)
+ 
+#SST_points_EUR = gridpp.nearest(ECMWF_points_EUR, BW_grid, sst[valid_points])        
+print(valid_points.shape)
+print(sst.shape)
+print((sst[valid_points]).shape)
+SST_points_EUR = gridpp.nearest(ECMWF_points_EUR, BW_grid, sst) 
