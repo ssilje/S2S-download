dc6686a466a2d8e3531d44f104c01857cd407a68
Benjamin Narum <b_narum@outlook.com>
2021-03-22 17:12:52 +0100

    Dates given both for mon and thu possible

--- S2S/date_helpers.py
+++ S2S/date_helpers.py
@@ -4,7 +4,7 @@ def get_forcast_date_cycle(
         start_year,
         start_month,
         start_day,
-        num_periods,
+        num_weeks,
 ):
 
     start_date = pd.to_datetime(f"{start_year:04}-{start_month:02}-{start_day:02}")
@@ -12,8 +12,17 @@ def get_forcast_date_cycle(
     if start_date.weekday() != 0:
         raise AttributeError("Date is not a Monday. Forecasts start Mondays.")
 
-    return pd.date_range(
+    dr_mondays = pd.date_range(
         start=start_date,
-        periods=num_periods,
-        freq='7D'
+        periods=num_weeks,
+        freq='W-MON'
     )
+    return dr_mondays
+
+    # dr_thursdays = pd.date_range(
+    #     start=start_date,
+    #     periods=num_weeks,
+    #     freq='W-THU'
+    # )
+
+    # return dr_mondays.union(dr_thursdays)
