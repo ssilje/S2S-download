42dd6e2e2a993c7529da7021cee6e7b58846e918
henrikauestad <heau@norceresearch.no>
2021-06-07 11:55:40 +0200

    add test

--- test_henrik.py
+++ test_henrik.py
@@ -3,6 +3,11 @@
 import scripts.Henrik.create_domain_file
 # import S2S.organize_IMR
 
+# from S2S.organize_barentzwatch import organize_files
+# organize_files()
+
 # import scripts.Henrik.vpf_rawEC
-import scripts.Henrik.vpf_ppEC_all_points
+import scripts.Henrik.vpf_ppEC_test
 # import scripts.Henrik.vpf_combo_ERA
+
+import scripts.Henrik.aggregate_skill
