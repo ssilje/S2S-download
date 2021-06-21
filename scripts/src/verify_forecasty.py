68dd96ec2589462f9380bb70fcb37cf8cadf1877
Silje Sørland <ssor@norceresearch.no>
2021-06-22 00:40:03 +0200

    Update verify_forecasty.py

--- scripts/src/verify_forecasty.py
+++ scripts/src/verify_forecasty.py
@@ -43,6 +43,7 @@ process_era          = False
 make_time_series     = False
 
 high_res             = False
+verify               = True
 
 # steps = pd.to_timedelta([7,14,23,30,37,44],'D')
 steps = pd.to_timedelta([7, 14, 21, 28, 35, 42],'D')
@@ -69,6 +70,39 @@ random_fc = xr.open_dataset(config['VALID_DB']+path_e+\
 random_fc_a = xr.open_dataset(config['VALID_DB']+path_e+\
                                 long_name + '_random-forecast_anomalies.nc')[var]
 
+
+
+
+stacked_era = xh.assign_validation_time(stacked_era)
+stacked_era_a = xh.assign_validation_time(stacked_era_a)
+hindcast = xh.assign_validation_time(hindcast)
+hindcast_a = xh.assign_validation_time(hindcast_a)
+clim_mean = xh.assign_validation_time(clim_mean)
+clim_std = xh.assign_validation_time(clim_std)
+random_fc = xh.assign_validation_time(random_fc)
+random_fc_a = xh.assign_validation_time(random_fc_a)
+    
+    
+if verify:
+    val_obs = stacked_era
+    clim_fc  = models.clim_fc(clim_mean,clim_std)
+    hindcast = hindcast.sel(time=slice(val_obs.time.min(),val_obs.time.max()))
+    
+    
+
+    crps_mod  = ps.crps_ensemble(np.moveaxis(val_obs.values,0,1),np.moveaxis(hindcast.values,0,-1))
+    
+    #crps_mod  = xs.crps_ensemble(np.moveaxis(val_obs.values,0,1),hindcast.values,dim=[]) fekke ein feilmelding
+    crps_clim      = ps.crps_gaussian(x=sst_obs, mu=0, sig=std_obs) # does not weight with cos(lat), might be problematic in next operation crps_SS
+    crps_clim = ps.crps_gaussian(
+                                np.moveaxis(stacked_era_a.values,0,1),
+                                np.moveaxis(clim_mean.values,-1,1),
+                                np.moveaxis(clim_std.values,-1,1),
+                                dim=[]
+                                )
+
+    gr.skill_plot(crps_mod,crps_clim,title='rawEC',filename='rawEC')
+    gr.qq_plot(val_obs[var],hindcast[var],y_axlabel='raw EC',filename='rawEC')
 
 
 stacked_era = xh.assign_validation_time(
