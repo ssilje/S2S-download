945580542c548c439d402bedd771da54fcb5566c
henrikauestad <heau@norceresearch.no>
2021-06-29 08:28:50 +0200

    demonstrate

--- demonstrate_modeling.py
+++ demonstrate_modeling.py
@@ -16,7 +16,7 @@ clim_t_start  = (2000,1,1)
 clim_t_end    = (2021,1,4)
 
 
-high_res = False
+high_res = True
 steps    = pd.to_timedelta([9,16,23,30,37],'D')
 
 grid_hindcast     = Hindcast(
--- demonstrate_timeseries.py
+++ demonstrate_timeseries.py
@@ -16,7 +16,7 @@ clim_t_start  = (2000,1,1)
 clim_t_end    = (2021,1,4)
 
 
-high_res = False
+high_res = True
 steps    = pd.to_timedelta([9,16,23,30,37],'D')
 
 grid_hindcast = Hindcast(
@@ -43,7 +43,7 @@ point_observations = Observations(
                             name='BarentsWatch',
                             observations=point_observations,
                             forecast=grid_hindcast,
-                            process=False
+                            process=True
                             )
 
 point_hindcast = Grid2Point(point_observations,grid_hindcast).correlation(
