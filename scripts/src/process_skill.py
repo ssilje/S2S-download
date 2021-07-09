c684e8ff3ddac7b2b27d5440e68cbb43a5661be8
Silje Sørland <ssor@norceresearch.no>
2021-07-09 10:44:05 +0200

    Update process_skill.py

--- scripts/src/process_skill.py
+++ scripts/src/process_skill.py
@@ -207,7 +207,7 @@ grid_hindcast = Hindcast(
                         bounds,
                         high_res=high_res,
                         steps=steps,
-                        process=True,
+                        process=False,
                         download=False,
                         split_work=True
                     )
