3cba7df4e77dbf5d9c32455eb56bcbe0230d9f9c
Silje Sørland <ssor@norceresearch.no>
2021-07-16 17:04:29 +0200

    Update process_hindcast_forecast.py

--- scripts/SUMMER2018/process_hindcast_forecast.py
+++ scripts/SUMMER2018/process_hindcast_forecast.py
@@ -31,40 +31,64 @@ clim_t_end      = (2019,12,31)
 
 high_res        = False
 
-#steps           = pd.to_timedelta([7, 14, 21, 28, 35, 42],'D')
-steps           = pd.to_timedelta(np.linspace(1,46,46),'D')
+steps           = pd.to_timedelta([7, 14, 21, 28, 35, 42],'D')
+#steps           = pd.to_timedelta(np.linspace(1,46,46),'D')
 
 print('\tProcess Forecast')
+#grid_forecast = Forecast(
+#                        var,
+#                        t_start,
+#                        t_end,
+#                        bounds,
+#                        high_res=high_res,
+#                        steps=steps,
+#                        process=False,
+#                        download=False,
+#                        split_work=True
+#                    )
+
 grid_forecast = Forecast(
                         var,
                         t_start,
                         t_end,
                         bounds,
                         high_res=high_res,
-                        steps=steps,
-                        process=False,
+                        process=True,
                         download=False,
                         split_work=True
                     )
 
 
+
 print('\tProcess Hindcast')
+#grid_hindcast = Hindcast(
+#                        var,
+#                        t_start,
+#                        t_end,
+#                        bounds,
+#                        high_res=high_res,
+#                        steps=steps,
+#                        process=False,
+#                        download=False,
+#                        split_work=True
+#                    )
+
 grid_hindcast = Hindcast(
                         var,
                         t_start,
                         t_end,
                         bounds,
                         high_res=high_res,
-                        steps=steps,
-                        process=False,
+                        process=True,
                         download=False,
                         split_work=True
                     )
 
-
+## Sjekk - dataene blir berre nan. loopen under ser ok ut. 
 hc_fc = []
-hc_fc.append(grid_forecast.data)
 hc_fc.append(grid_hindcast.data)
+hc_fc.append(grid_forecast.data)
+
 hindcast_full = xr.concat(hc_fc,dim='time') ## stacking the data along month dimension
 hindcast_full = hindcast_full.rename(var)
 
@@ -91,7 +115,7 @@ forecast           = xh.assign_validation_time(grid_forecast.data)
 
 
 #for n,(nn,fcdata_m) in enumerate(fc_member): # loop through each member
-  
+fcc_step = []    
 for lt in steps:
       fc_steps          = forecast.sel(step=pd.Timedelta(lt,'D')) #loop through each month
       hc_steps          = hindcast.sel(step=pd.Timedelta(lt,'D'))
@@ -99,7 +123,7 @@ for lt in steps:
       dim               = 'validation_time.month'
       fc_group          = list(fc_steps.groupby(dim)) 
       hc_group          = list(hc_steps.groupby(dim))
-        
+      fcc_month = []  
       for m,(mf,fcdata) in enumerate(fc_group): #loop through each month
           mh,hcdata          = hc_group[m]
             
@@ -107,12 +131,25 @@ for lt in steps:
           fc_group_day        = list(fcdata.groupby(dim)) # lagar en liste for kvar mnd (nr_mnd, xarray)
           hc_group_day        = list(hcdata.groupby(dim))
           
+          fcc_day = []
           for mm,(mmf,fcdata_m) in enumerate(fc_group_day): #loop through each month
               mmh,hcdata_m          = hc_group_day[mm]
-              
-              for 
-              
-              hcdata_m.mean('time').mean('member') - fcdata_m.mean('member')
+             
+              fcc = []
+              for ensm in range(0,51,1):
+                  fc_anom = hcdata_m.mean('time').mean('member') - fcdata_m.sel(member=ensm)
+                  
+                  fcc.append(fc_anom)
+              fcc_member = xr.concat(fcc,dim='member')    
+              fcc_day.append(fcc_member)
+          fcc_member_day = xr.concat(fcc_day,dim='time')
+          fcc_month.append(fcc_member_day)
+      
+      fcc_member_day_month = xr.concat(fcc_month,dim='time')
+      fcc_step.append(fcc_member_day_month)
+  forecast_anom = xr.concat(ffcc_step,dim='step')    
+      
+                 # fc_anom=fc_anom.assign_coords(time_month=xlabel)
           
 
     
