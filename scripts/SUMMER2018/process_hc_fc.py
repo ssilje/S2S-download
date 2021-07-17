13e60384e7be45cf244655297d1e90ea3b06c21b
Silje Sørland <ssor@norceresearch.no>
2021-07-17 15:17:50 +0200

    Update process_hc_fc.py

--- scripts/SUMMER2018/process_hc_fc.py
+++ scripts/SUMMER2018/process_hc_fc.py
@@ -116,26 +116,41 @@ forecast           = xh.assign_validation_time(grid_forecast.data)
 
 
 #for n,(nn,fcdata_m) in enumerate(fc_member): # loop through each member
-fcc_step = []    
+fcc_step = []  
+hcc_step = []
+re_step  = []
 for lt in steps:
     fc_steps          = forecast.sel(step=pd.Timedelta(lt,'D')) #loop through each month
     hc_steps          = hindcast.sel(step=pd.Timedelta(lt,'D'))
+    re_steps          = reanalysis.sel(step=pd.Timedelta(lt,'D'))   
         
     dim               = 'validation_time.month'
+    
     fc_group          = list(fc_steps.groupby(dim)) 
     hc_group          = list(hc_steps.groupby(dim))
-    fcc_month = []  
+    re_group          = list(re_steps.groupby(dim))
+    
+    fcc_month = []
+    hcc_month = []
+    re_month  = [] 
     
     for m,(mf,fcdata) in enumerate(fc_group): #loop through each month
         mh,hcdata          = hc_group[m]
+        mr,redata          = re_group[m]
             
         dim                 = 'validation_time.day'
+        
         fc_group_day        = list(fcdata.groupby(dim)) # lagar en liste for kvar mnd (nr_mnd, xarray)
         hc_group_day        = list(hcdata.groupby(dim))
+        re_group_day        = list(redata.groupby(dim))
           
         fcc_day = []
+        hcc_day = []
+        rcc_day = []
+        
         for mm,(mmf,fcdata_m) in enumerate(fc_group_day): #loop through each month
             mmh,hcdata_m          = hc_group_day[mm]
+            mmr,redata_m          = re_group_day[mm]
              
             fcc = []
             for ensm in range(0,51,1):
@@ -144,12 +159,41 @@ for lt in steps:
                 fcc.append(fc_anom)
             fcc_member = xr.concat(fcc,dim='member')    
             fcc_day.append(fcc_member)
+            
+            hcc = []
+            for ensm in range(0,11,1):
+                hc_anom = hcdata_m.mean('time').mean('member') - hcdata_m.sel(member=ensm)
+                  
+                hcc.append(hc_anom)
+            hcc_member = xr.concat(hcc,dim='member')    
+            hcc_day.append(hcc_member)
+            
+            re_anom = redata_m.mean('time') - redata_m
+            rcc_day.append(re_anom)
+            
         fcc_member_day = xr.concat(fcc_day,dim='time')
         fcc_month.append(fcc_member_day)
+        
+        hcc_member_day = xr.concat(hcc_day,dim='time')
+        hcc_month.append(hcc_member_day)
+        
+        rcc_member_day = xr.concat(rcc_day,dim='time')
+        re_month.append(rcc_member_day)
       
     fcc_member_day_month = xr.concat(fcc_month,dim='time')
     fcc_step.append(fcc_member_day_month)
-forecast_anom = xr.concat(fcc_step,dim='step')    
+    
+    hcc_member_day_month = xr.concat(hcc_month,dim='time')
+    hcc_step.append(hcc_member_day_month)
+    
+    re_member_day_month = xr.concat(re_month,dim='time')
+    re_step.append(re_member_day_month)
+    
+    
+    
+    
+forecast_anom = xr.concat(fcc_step,dim='step')
+hindcast_anom = xr.concat(hcc_step,dim='step') 
       
                  # fc_anom=fc_anom.assign_coords(time_month=xlabel)
           
