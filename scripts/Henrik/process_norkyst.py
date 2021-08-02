c3825fac1a3eccb7813adf3bfe6373ab06667928
henrikauestad <henrik.stormark.auestad@gmail.com>
2021-08-02 10:34:45 +0200

    uw4 #norkyst

--- scripts/Henrik/process_norkyst.py
+++ scripts/Henrik/process_norkyst.py
@@ -119,9 +119,6 @@ for n,date in enumerate(date_range):
                 )
 
             out = out.assign_coords(radius=r)
-            out = out.assign_coords(time=pd.Timestamp(date))
-            print(out)
-            exit()
 
             if download:
                 out.to_netcdf(filename_nird)
--- scripts/Henrik/verify2norkyst.py
+++ scripts/Henrik/verify2norkyst.py
@@ -1,14 +1,31 @@
 import xarray as xr
+import pandas as pd
 
-# get observations
-path     = '/nird/projects/NS9853K/DATA/norkyst800/'
-filename = 'norkyst800_sst_*_daily_mean_at-BW-loc.nc'
+# # get observations
+# path     = '/nird/projects/NS9853K/DATA/norkyst800/'
+# filename = 'norkyst800_sst_*_daily_mean_at-BW-loc.nc'
+#
+# ds1 = xr.open_dataset(
+#                 path + 'norkyst800_sst_2016-11-12_daily_mean_at-BW-loc.nc'
+#             )
+# print(ds1)
+# exit()
+# ds = xr.open_mfdataset( path + filename )
+#
+# print(ds)
 
-ds1 = xr.open_dataset(
-                path + 'norkyst800_sst_2016-11-12_daily_mean_at-BW-loc.nc'
-            )
-print(ds1)
-exit()
-ds = xr.open_mfdataset( path + filename )
+##################################################################
+start_date = '2012-06-27'
+end_date   = '2019-02-26'
 
-print(ds)
+date_range = pd.date_range(start=start_date,end=end_date,freq='D')
+
+for n,date in enumerate(date_range):
+
+    filename_nird = 'norkyst800_sst_'+date.strftime('%Y-%m-%d')+\
+                        '_daily_mean_at-BW-loc.nc'
+
+    ds = xr.open_dataset( path + filename_nird )
+
+    print(ds)
+    exit()
