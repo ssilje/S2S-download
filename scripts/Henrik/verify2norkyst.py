e32666b8ce46f03d28e28f39cdf69575bfcaa95b
henrikauestad <henrik.stormark.auestad@gmail.com>
2021-08-02 10:25:33 +0200

    uw2 #norkyst

--- scripts/Henrik/verify2norkyst.py
+++ scripts/Henrik/verify2norkyst.py
@@ -4,6 +4,11 @@ import xarray as xr
 path     = '/nird/projects/NS9853K/DATA/norkyst800/'
 filename = 'norkyst800_sst_*_daily_mean_at-BW-loc.nc'
 
+ds1 = xr.open_dataset(
+                path + 'norkyst800_sst_2016-11-12_daily_mean_at-BW-loc.nc'
+            )
+print(ds1)
+exit()
 ds = xr.open_mfdataset( path + filename )
 
 print(ds)
