03096f81946e4451356e30d90b5ce96fa14d1edd
henrikauestad <henrik.stormark.auestad@gmail.com>
2021-08-02 16:38:17 +0200

    uw14 #norkyst

--- scripts/Henrik/verify2norkyst.py
+++ scripts/Henrik/verify2norkyst.py
@@ -3,37 +3,37 @@ import pandas as pd
 
 # # get observations
 path     = '/nird/projects/NS9853K/DATA/norkyst800/'
-# filename = 'norkyst800_sst_*_daily_mean_at-BW.nc'
-#
-# ds = xr.open_mfdataset( path + filename )
-#
-# print(ds)
+filename = 'norkyst800_sst_*_daily_mean_at-BW.nc'
+
+ds = xr.open_mfdataset( path + filename )
+
+print(ds)
 
 ##################################################################
-start_date = '2012-06-27'
-end_date   = '2019-02-26'
-
-date_range = pd.date_range(start=start_date,end=end_date,freq='D')
-
-for n,date in enumerate(date_range):
-
-    filename_nird = 'norkyst800_sst_'+date.strftime('%Y-%m-%d')+\
-                        '_daily_mean_at-BW-loc.nc'
-
-    filename_out = 'norkyst800_sst_'+date.strftime('%Y-%m-%d')+\
-                        '_daily_mean_at-BW.nc'
-
-    print(filename_nird)
-
-    try:
-
-        ds = xr.open_dataset( path + filename_nird )
-        ds = ds.assign_coords(time=[pd.Timestamp(date)])
-        ds = ds.to_array().rename('sst').isel(variable=0).drop('variable')
-
-        ds.to_netcdf(path+filename_out)
-        print('\n')
-
-    except FileNotFoundError:
-        print('Not found\n')
-        pass
+# start_date = '2012-06-27'
+# end_date   = '2019-02-26'
+#
+# date_range = pd.date_range(start=start_date,end=end_date,freq='D')
+#
+# for n,date in enumerate(date_range):
+#
+#     filename_nird = 'norkyst800_sst_'+date.strftime('%Y-%m-%d')+\
+#                         '_daily_mean_at-BW-loc.nc'
+#
+#     filename_out = 'norkyst800_sst_'+date.strftime('%Y-%m-%d')+\
+#                         '_daily_mean_at-BW.nc'
+#
+#     print(filename_nird)
+#
+#     try:
+#
+#         ds = xr.open_dataset( path + filename_nird )
+#         ds = ds.assign_coords(time=[pd.Timestamp(date)])
+#         ds = ds.to_array().rename('sst').isel(variable=0).drop('variable')
+#
+#         ds.to_netcdf(path+filename_out)
+#         print('\n')
+#
+#     except FileNotFoundError:
+#         print('Not found\n')
+#         pass
