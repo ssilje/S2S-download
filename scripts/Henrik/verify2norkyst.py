3f0716be52dd1f967c3dd1255e70928b7252d653
henrikauestad <henrik.stormark.auestad@gmail.com>
2021-08-03 10:04:20 +0200

    yet another

--- scripts/Henrik/verify2norkyst.py
+++ scripts/Henrik/verify2norkyst.py
@@ -52,7 +52,7 @@ for loc_name in location_names:
 
     data.append(xr.open_dataset(fname)[var])
 
-point_observations = xr.concat(data,'location',join='outer')
+point_observations = xr.concat(data,'location',join='outer').drop('radius')
 
 print( point_observations.time )
 
