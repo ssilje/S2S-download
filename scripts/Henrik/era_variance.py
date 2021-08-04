2fc1d0a888d3f5bf20d48e06509750316a1714ef
henrikauestad <henrik.stormark.auestad@gmail.com>
2021-08-04 14:09:23 +0200

    commit

--- scripts/Henrik/era_variance.py
+++ scripts/Henrik/era_variance.py
@@ -24,7 +24,7 @@ clim_t_end    = (2021,1,4)
 high_res = True
 
 era = ERA5(high_res=True).load(
-                            var         = var
+                            var         = var,
                             start_time  = clim_t_start,
                             end_time    = clim_t_end,
                             bounds      = bounds
