feb670978c47e43c7bc123456168d67ac69869bb
henrikauestad <henrik.stormark.auestad@gmail.com>
2021-08-04 14:23:45 +0200

    commit

--- scripts/Henrik/era_variance.py
+++ scripts/Henrik/era_variance.py
@@ -23,21 +23,20 @@ clim_t_end    = (2021,1,4)
 
 high_res = True
 
+mparser = {
+            '01':'JAN','02':'FEB','03':'MAR',
+            '04':'APR','05':'MAY','06':'JUN',
+            '07':'JUL','08':'AUG','09':'SEP',
+            '10':'OCT','11':'NOV','12':'DEC'
+        }
+months  = ['01','02','03','04','05','06','07','08','09','10','11','12']
+
 era = ERA5(high_res=True).load(
                             var         = var,
                             start_time  = clim_t_start,
                             end_time    = clim_t_end,
                             bounds      = bounds
                         )
-print(era)
-exit()
-path   = '/nird/projects/NS9853K/DATA/norkyst800/'
-# fn1    = 'norkyst800_sst_'
-# fn2    = '_var.nc'
-fname1 = 'norkyst800_sst_'
-fname2 = '_daily_mean.nc'
-
-months = ['01','02','03','04','05','06','07','08','09','10','11','12']
 
 # latex.set_style(style='white')
 # fig,axes = plt.subplots(1,1,\
@@ -53,33 +52,30 @@ for month in months:
         figsize=latex.set_size(width=345,subplots=(1,1),fraction=0.95),\
         subplot_kw=dict(projection=ccrs.NorthPolarStereo()))
 
-    fname = fname1 + '*-' + month + '-*' + fname2
-    print(fname)
+    print(month)
 
-    try:
-        # data = xr.open_dataset(path+fn1+month+fn2).squeeze()
-        with xr.open_mfdataset( path + fname, parallel=True ) as data:
+    print(era.time.dt.month)
 
-            print(data)
-            # # load to memory
-            data = data.load()
-            print(data)
-            data = data.temperature.var('time',skipna=True).squeeze()
+    exit()
 
-            lons = data.longitude
-            lats = data.latitude
 
-            cmap   = latex.cm_rgc(c='yellow')
-            levels = np.arange(0,7,0.5)
-            norm   = BoundaryNorm(levels,cmap.N)
+    print(data)
+    # # load to memory
+    data = data.load()
+    print(data)
+    data = data.temperature.var('time',skipna=True).squeeze()
 
-            cs = ax.contourf(lons,lats,data,transform=ccrs.PlateCarree(),
-                                cmap=cmap,norm=norm,extend='max',levels=levels)
-            ax.coastlines(resolution='10m', color='grey',\
-                                    linewidth=0.2)
-            ax.set_title(month)
-            fig.colorbar(cs,ax=ax)
-            graphics.save_fig(fig,'variance_map_norkyst_'+month)
+    lons = data.longitude
+    lats = data.latitude
 
-    except (FileNotFoundError,MemoryError) as e:
-        pass
+    cmap   = latex.cm_rgc(c='yellow')
+    levels = np.arange(0,7,0.5)
+    norm   = BoundaryNorm(levels,cmap.N)
+
+    cs = ax.contourf(lons,lats,data,transform=ccrs.PlateCarree(),
+                        cmap=cmap,norm=norm,extend='max',levels=levels)
+    ax.coastlines(resolution='10m', color='grey',\
+                            linewidth=0.2)
+    ax.set_title(month)
+    fig.colorbar(cs,ax=ax)
+    graphics.save_fig(fig,'variance_map_norkyst_'+month)
