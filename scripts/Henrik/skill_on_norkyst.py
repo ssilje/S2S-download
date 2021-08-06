3ba0870d6b9006db07a54aa11ba601be2a864d00
henrikauestad <henrik.stormark.auestad@gmail.com>
2021-08-06 11:21:04 +0200

    commit

--- scripts/Henrik/skill_on_norkyst.py
+++ scripts/Henrik/skill_on_norkyst.py
@@ -62,7 +62,7 @@ for loc in bw.location.values:
                             download=False,
                             split_work=True
                         )
-    print(hindcast.data_a.step.isel(step=0).values)
+    print(hindcast.data_a.step.isel(step=0).dt.days)
     observations = Observations(
                                 name='NorKyst-800_'+str(loc),
                                 observations=nk,
