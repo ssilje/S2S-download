61fa735423c8210673fca4a69d77a0ede76c3a43
Henrik Auestad <54135476+henrikauestad@users.noreply.github.com>
2021-08-11 15:27:14 +0200

    Revert "Issue36"

--- S2S/local_configuration_H.py
+++ S2S/local_configuration_H.py
@@ -9,13 +9,9 @@ config = {
     "S2S_DIR_summer2018": '', # "/nird/projects/NS9001K/sso102/S2S/DATA/SUMMER2018",
     "SAVEFIG": 'figures/', # " /nird/projects/NS9001K/sso102/FIGUERS"
     'VALID_DB':'data/valid_db/',
-    'NORKYST':'data/norkyst800/',
-    'S2SF':'/nird/projects/NS9853K/DATA/S2S/forecast/ECMWF/sfc/',
-    'S2SH':'/nird/projects/NS9853K/DATA/S2S/hindcast/ECMWF/sfc/',
-    'ERA5':'/nird/projects/NS9853K/DATA/SFE/ERA_daily_nc/grid_15/',
-    # 'S2SF':'data/nird_copy/S2S/Benjamin/forecast/ECMWF/sfc/', # local to henrik
-    # 'S2SH':'data/nird_copy/S2S/Benjamin/hindcast/ECMWF/sfc/', # local to henrik
-    # 'ERA5':'data/nird_copy/SFE/ERA_daily_nc/grid_15/',  # local to henrik
+    'S2SF':'data/nird_copy/S2S/Benjamin/forecast/ECMWF/sfc/', # local to henrik
+    'S2SH':'data/nird_copy/S2S/Benjamin/hindcast/ECMWF/sfc/', # local to henrik
+    'ERA5':'data/nird_copy/SFE/ERA_daily_nc/grid_15/',  # local to henrik
     'SITES':'data/BW/sites.json',
     'DOMAINS':'data/EIDE/domains.json',
     'BW':'./data/BW/BW_temperature/',
--- S2S/xarray_helpers.py
+++ S2S/xarray_helpers.py
@@ -17,7 +17,7 @@ def print_progress(n,N,i='',e=''):
     """
     print('\t\t'+i+str(round((n/N)*100,1))+' % done'+e,end='\r')
 
-def running_clim(x,index,window=30,cross_validation=False):
+def running_clim(x,index,window=30):
     """
     Computes mean and standard deviation over x keeping dim -1. Dim -1 must be
     'dayofyear', with the corresponding days given in index.
@@ -51,46 +51,19 @@ def running_clim(x,index,window=30,cross_validation=False):
     index[-pad:] += index[-pad-1]
     index[:pad]  -= index[-pad-1]
 
-    if not cross_validation:
+    for ii,idx in enumerate(index[pad:-pad]):
 
-        for ii,idx in enumerate(index[pad:-pad]):
+        # pool all values that falls within window
+        pool = x[...,np.abs(index-idx)<=pad]
 
-            # pool all values that falls within window
-            pool = x[...,np.abs(index-idx)<=pad]
+        if np.isfinite(pool).sum() > 1:
+            mean.append(np.full_like(pool[0][...,0],np.nanmean(pool)))
+            std.append(np.full_like(pool[0][...,0],np.nanstd(pool)))
+        else:
+            mean.append(np.full_like(pool[0][...,0],np.nan))
+            std.append(np.full_like(pool[0][...,0],np.nan))
 
-            if np.isfinite(pool).sum() > 1:
-                mean.append(np.full_like(pool[0][...,0],np.nanmean(pool)))
-                std.append(np.full_like(pool[0][...,0],np.nanstd(pool)))
-            else:
-                mean.append(np.full_like(pool[0][...,0],np.nan))
-                std.append(np.full_like(pool[0][...,0],np.nan))
-
-        return np.stack(mean,axis=-1),np.stack(std,axis=-1)
-
-    else:
-
-        for ii,idx in enumerate(index[pad:-pad]):
-
-            # pool all values that falls within window
-            pool = x[...,np.abs(index-idx)<=pad]
-
-            ymean,ystd = [],[]
-            for yy in range(pool.shape[-2]):
-
-                # delete the relevant year from pool (for cross validation)
-                filtered_pool = np.delete(pool,yy,axis=-2)
-
-                if np.isfinite(filtered_pool).sum() > 1:
-                    ymean.append(np.nanmean(filtered_pool))
-                    ystd.append(np.nanstd(filtered_pool))
-                else:
-                    ymean.append(np.nan)
-                    ystd.append(np.nan)
-
-            mean.append(np.array(ymean))
-            std.append(np.array(ystd))
-
-        return np.stack(mean,axis=-1),np.stack(std,axis=-1)
+    return np.stack(mean,axis=-1),np.stack(std,axis=-1)
 
 def running_clim_CV(x,index,window=30):
     """
@@ -183,7 +156,7 @@ def o_climatology(da,window=30):
     # re-assing time dimension to da from year,dayofyear
     return stack_time(mean),stack_time(std)
 
-def c_climatology(da,window=30,cross_validation=False):
+def c_climatology(da):
     """
     Climatology with centered initialization time
     """
@@ -194,13 +167,8 @@ def c_climatology(da,window=30,cross_validation=False):
 
     # to all year,dayofyear matrices in da, apply runnning_clim
     mean,std = xr.apply_ufunc(
-            running_clim, da, da.dayofyear, window, cross_validation,
-            input_core_dims  = [
-                                ['member','year','dayofyear'],
-                                ['dayofyear'],
-                                [],
-                                []
-                            ],
+            running_clim, da, da.dayofyear,
+            input_core_dims  = [['member','year','dayofyear'],['dayofyear']],
             output_core_dims = [['year','dayofyear'],['year','dayofyear']],
             vectorize=True
         )
