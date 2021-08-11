b4c48a54a784d4ae6fd370acb5fcbdcd753893c8
Henrik Auestad <heau@login1-nird-tos.nird.login.svc.cluster.local>
2021-08-11 15:24:58 +0200

    Debugging issue36

--- test.py
+++ test.py
@@ -3,34 +3,26 @@ from S2S.data_handler import ERA5, ECMWF_S2SF, ECMWF_S2SH
 from S2S.process import Hindcast
 
 bounds = (5,10,60,65)
-variables    = ['sst']
+variables    = ['u10']
 
 t_start  = (2020,4,10)
-t_end    = (2020,4,20)
+t_end    = (2020,5,20)
 
 for var in variables:
     for high_res in [False]:
         for loader,label in zip([ECMWF_S2SH],['H']):
 
-            print('\n')
-            print('Variable:',var)
-            print('High res:',high_res)
-            print('Type:',label)
+            data = Hindcast(
+                    var,
+                    t_start,
+                    t_end,
+                    bounds,
+                    high_res=high_res,
+                    process=True,
+                    download=False,
+                    split_work=False,
+                    cross_val=False
+                )
 
-            try:
-
-                Hindcast(
-                        var,
-                        t_start,
-                        t_end,
-                        bounds,
-                        high_res=high_res,
-                        process=False,
-                        download=False,
-                        split_work=False
-                    )
-
-                print('Success')
-
-            except (KeyError,ValueError) as e:
-                print(repr(e))
+            print(data.mean)
+            print(data.std)
