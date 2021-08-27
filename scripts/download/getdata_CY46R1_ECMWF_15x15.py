e5f3256c889e33e49bcea7445bf3585c6ede5685
Silje Sørland <ssor@norceresearch.no>
2021-08-27 16:34:36 +0200

    Update getdata_CY46R1_ECMWF_15x15.py

--- scripts/download/getdata_CY46R1_ECMWF_15x15.py
+++ scripts/download/getdata_CY46R1_ECMWF_15x15.py
@@ -3,6 +3,7 @@ from ecmwfapi import ECMWFDataServer
 import os,sys
 import pandas as pd
 from datetime import datetime
+import S2S.date_to_model  as d2m
 
 server = ECMWFDataServer()
 
@@ -10,7 +11,7 @@ product = 'hindcast' # forecast, hincast
 dirbase = 'local_path'
 dir = '%s/%s/%s/'%(dirbase,product,'/ECMWF/sfc')
 
-forcastcycle = 'CY46R1'
+
 
 if product == 'hindcast':
     STREAM =  'enfh',
@@ -94,6 +95,7 @@ for filename in (
             if not os.path.exists(datadir)  :
                 os.makedirs(datadir)
             hdate = '/'.join([d.replace('%i'%refyear,'%i'%i) for i in range(refyear-20,refyear)])
+            forcastcycle = d2m.which_mv_for_init(d,model='ECMWF',fmt='%Y-%m-%d')
             target = '%s/%s_%s_%s_%s_%s.grb'%(datadir,filename,forcastcycle,d,prefix,product)
             if not os.path.isfile(target):
                dic = basedict.copy()
